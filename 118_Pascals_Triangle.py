158425e535150379ce98da65972fbd7a805a47f3
Pallab Sarker <155830923+pallabsarker@users.noreply.github.com>
2024-08-19 22:12:19 +0600

    Update 118_Pascals_Triangle.py

--- 118_Pascals_Triangle.py
+++ 118_Pascals_Triangle.py
@@ -13,3 +13,18 @@ class Solution:
         return output
     
 print(Solution().generate(5))
+
+# List = list
+# class Solution:
+#     def generate(self, numRows: int) -> List[List[int]]:
+        
+#         output = [[1]]
+#         for i in range(2, numRows + 1):
+#             element = [1]
+#             for j in range(1, i - 1):
+#                 element.append(output[-1][j] + output[-1][j - 1])
+#             element.append(1)
+#             output.append(element)
+#         return output
+    
+# print(Solution().generate(4))
